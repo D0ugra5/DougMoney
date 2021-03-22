27ee449278058d0fc388ae7e37dd5ac75462cd42
Douglas <douglasmb87@gmail.com>
2021-03-22 13:33:34 -0300

    'global_css_aplicado'

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
-
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-     <h1>Hello World</h1>
+  <h1>Hello World</h1>
+  <GlobalStyle></GlobalStyle>
     </div>
   );
 }
-
